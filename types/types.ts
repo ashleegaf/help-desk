a0ac40485b7047cbbc63a270135d366646768ad6
ashleegaf <ashleegaf@gmail.com>
2023-08-01 00:54:54 -0400

    Define types for detailed tickets, panel data, and agent data

--- types/types.ts
+++ types/types.ts
@@ -10,8 +10,43 @@ export interface Ticket {
   updatedAt: Date;
 }
 
+export interface DetailedTicket {
+  id: number;
+  userId: number;
+  subject: string;
+  description: string;
+  status: string;
+  lastMsg: string;
+  agentId: number | null;
+  createdAt: string;
+  updatedAt: string;
+  user: {
+    name: string;
+    email: string;
+  };
+  agent: {
+    id?: number | undefined;
+    name: string;
+  };
+}
+
+export interface PanelData {
+  detailedTickets: DetailedTicket[];
+  filteredAgents: Agent[];
+}
+
 export interface TicketUpdates {
   agentData: Record<string, any> | null;
   statusData: Record<string, any> | null;
   emailDate: Record<string, any> | null;
+}
+
+export interface Agent {
+  id: number;
+  name: string;
+}
+
+export interface AgentItems {
+  key: string;
+  label: string;
 }
