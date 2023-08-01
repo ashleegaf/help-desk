aad209d2b8e7ee0d09dbe748a1927b55b3f6b559
ashleegaf <ashleegaf@gmail.com>
2023-07-31 20:43:44 -0400

    Create seed data for all models

---
+++ prisma/data/agent.js
@@ -0,0 +1,28 @@
+// cspell:disable
+module.exports = [
+  {
+    name: 'Epcot Troop',
+    email: 'eco@troop.com',
+    password: 'hiepcot',
+  },
+  {
+    name: 'Tempe Sow',
+    email: 'tempe@sow.com',
+    password: 'hitempe',
+  },
+  {
+    name: 'Miami Blue',
+    email: 'miami@blue.com',
+    password: 'himiami',
+  },
+  {
+    name: 'Fran Riz',
+    email: 'fran@riz.com',
+    password: 'hifran',
+  },
+  {
+    name: 'True Bark',
+    email: 'true@bark.com',
+    password: 'hitrue',
+  },
+];
---
+++ prisma/data/users.js
@@ -0,0 +1,150 @@
+// cspell:disable
+module.exports = [
+  {
+    name: 'Apple Lor',
+    email: 'apple@apple.com',
+    ticket: {
+      create: [
+        {
+          subject: 'Login issue',
+          description: 'Trouble logging in.',
+          status: 'In Progress',
+          agentId: 1
+        },
+        {
+          subject: 'Nisl nisi scelerisque',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut morbi tincidunt augue interdum velit euismod. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus.',
+          status: 'Resolved',
+          agentId: 2
+        },
+        {
+          subject: 'Aenean vel elit scel',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          status: 'In Progress',
+          agentId: 3
+        },
+        {
+          subject: 'Nunc aliquet bibendu',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          status: 'Resolved',
+          agentId: 4
+        },
+        {
+          subject: 'Tristique risus nec',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          status: 'In Progress',
+          agentId: 5
+        },
+      ],
+    },
+  },
+  {
+    name: 'Green Hoop',
+    email: 'green@green.com',
+    ticket: {
+      create: [
+        {
+          subject: 'Password issue',
+          description: 'Forgot password.',
+          status: 'In Progress',
+          agentId: 1
+        },
+        {
+          subject: 'Email issue',
+          description: 'Forgot email.',
+          status: 'In Progress',
+          agentId: 2
+        },
+        {
+          subject: 'Feugiat in fermentum',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          status: 'In Progress',
+          agentId: 3
+        },
+        {
+          subject: 'Nisl tincidunt eget',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          status: 'In Progress',
+          agentId: 4
+        },
+        {
+          subject: 'Nullam non nisi',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          status: 'In Progress',
+          agentId: 5
+        },
+      ],
+    },
+  },
+  {
+    name: 'Berry Nite',
+    email: 'berry@berry.com',
+    ticket: {
+      create: [
+        {
+          subject: 'Service issue',
+          description: 'Problem using services.',
+          status: 'Resolved',
+          agentId: 1
+        },
+        {
+          subject: 'Test issue',
+          description: 'Test functionality.',
+          status: 'Resolved',
+          agentId: 2
+        },
+        {
+          subject: 'Tempus iaculis urna',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          status: 'Resolved',
+          agentId: 3
+        },
+        {
+          subject: 'Nulla facilisi morbi',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          status: 'Resolved',
+          agentId: 4
+        },
+        {
+          subject: 'Amet purus gravida',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          status: 'Resolved',
+          agentId: 5
+        },
+      ],
+    },
+  },
+  {
+    name: 'Rain Bliss',
+    email: 'rain@rain.com',
+    ticket: {
+      create: [
+        {
+          subject: 'Service issue',
+          description: 'Problem using services.',
+          agentId: 1
+        },
+        {
+          subject: 'Test issue',
+          description: 'Test functionality.',
+          agentId: 2
+        },
+        {
+          subject: 'Dolor magna aliqua',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          agentId: 3
+        },
+        {
+          subject: 'Sed do eiusmod',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          agentId: 4
+        },
+        {
+          subject: 'Incididunt ut',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          agentId: 5
+        },
+      ],
+    },
+  },
+];
